eb92c42adc5a2906285318f1612ef85d3e3e80be
Edwin <ed.galeano1@gmail.com>
2020-11-27 17:37:58 -0500

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tutorial';
+  // title = 'tutorial';
+  users = ['rafael', 'miguel', 'don', 'leonardo', 'splinter']
 }
