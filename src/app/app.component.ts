304a939d8c2d67698a2559984a5291dac9b09a0c
Edwin <ed.galeano1@gmail.com>
2020-11-28 16:51:44 -0500

    Ejemplo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'tutorial';
-  users = ['rafael', 'miguel', 'don', 'leonardo', 'splinter']
+  // users = ['rafael', 'miguel', 'don', 'leonardo', 'splinter']
+  name: string = "Ryan Ray";
+  age: number | undefined;
+  address: {
+    street: string;
+    city: string;
+  };
+  hobbies: string[];
+
+  constructor(){
+    this.age = 28;
+    this.address = {
+      street: '221B Baker Street',
+      city: 'London'
+    }
+    this.hobbies = ['swimming', 'read', 'write']; 
+  }
 }
